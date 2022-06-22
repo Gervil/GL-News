88676398fa9454b5186ec011f14ebe4ca99603a9
Gerardo Villarroel <gerardovil@gmail.com>
2022-06-22 11:59:01 -0400

    Initial Commit

---
+++ GlobalLogic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GlobalLogic
+//
+//  Created by Gerardo Villarroel on 22-06-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
